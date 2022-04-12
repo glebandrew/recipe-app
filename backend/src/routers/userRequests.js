f32271faa9e82c8e2da662a29c73070a4e8edfcf
slin4er <slin4eria@gmail.com>
2022-04-12 14:51:24 +0200

    new routes

---
+++ backend/src/routers/userRequests.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const router = express.Router()
+const auth = require('../middlewares/auth')
+const {getFavRecipes, getUsersRecipes} = require('../service/recipeReq')
+const {getProfile} = require('../service/userReq')
+const {signInRequest, signOutRequest, signUpRequest} = require('../service/userReq')
+
+router.post('/signin', signInRequest)
+router.post('/signup', signUpRequest)
+router.post('/signout', auth, signOutRequest)
+router.get('/profile', auth, getProfile)
+router.get('/favorite/recipe', auth, getFavRecipes)
+router.get('/recipes', auth, getUsersRecipes)
+
+module.exports = router
